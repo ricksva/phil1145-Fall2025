26f6887c7ddb14fe39269c25a8915143f1a3138d
Vance Ricks <126216160+ricksva@users.noreply.github.com>
2025-08-29 11:35:58 -0400

    Delete Examples directory

--- Examples/Example 2.2 Singleton/Clock.ts
+++
@@ -1,9 +0,0 @@
-import {IClock} from './IClock'
-
-class Clock implements IClock {
-    private ticks = 0
-    public reset():void { this.ticks = 0 }
-    public tick():void { this.ticks++ }
-    public currentTime():number { return this.ticks }
-
-}
--- Examples/Example 2.2 Singleton/IClock.ts
+++
@@ -1,9 +0,0 @@
-export interface IClock {
-    // reset the tick counter to 0
-    reset(): void
-    // increment the tick counter
-    tick(): void
-    // returns the number of ticks since the last reset.
-    currentTime(): number
-}
-
